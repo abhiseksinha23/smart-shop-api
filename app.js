dd610c8b7ce8982533d86bfb6a4e856a3f0b23f9
Abhisek Kumar <42487773+abhiseksinha23@users.noreply.github.com>
2020-02-19 14:50:25 +0530

    change 8

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const bodyparser = require('body-parser');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 const cors = require('cors');
-//const MongoClient = require('mongodb').MongoClient;
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://smart-shop-admin:password1234@smart-shop-db-drot2.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
 
 
 app.use(express.static("public"));
@@ -25,43 +29,43 @@ app.use(cors());
 
 //const MongoClient = require('mongodb').MongoClient;
 //const uri = "mongodb+srv://smart-shop-admin:password1234@smart-shop-db-drot2.mongodb.net/test?retryWrites=true&w=majority";
-mongoose.connect("mongodb+srv://smart-shop-admin:<password>@smart-shop-db-drot2.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect("mongodb+srv://smart-shop-admin:<password>@smart-shop-db-drot2.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 
-const productschema = new mongoose.Schema({
-    name: String,
-    brand: String,
-    type: String,
-    category: String,
-    count: Number,
-    price: Number,
-    image: String,
-    description: String
-});
+// const productschema = new mongoose.Schema({
+//     name: String,
+//     brand: String,
+//     type: String,
+//     category: String,
+//     count: Number,
+//     price: Number,
+//     image: String,
+//     description: String
+// });
 
-//const client = new MongoClient(uri, { useNewUrlParser: true });
-const product = mongoose.model("product", productschema);
+// //const client = new MongoClient(uri, { useNewUrlParser: true });
+// const product = mongoose.model("product", productschema);
 
 
-const userschema = new mongoose.Schema({
-    name: String,
-    email: { type: String, unique: true, required: true },
-    password: String,
-    products: [{
-        productRef: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "product"
-        },
-        count: Number
-    }],
-    cart: [{
-        productRef: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "product"
-        },
-        count: Number
-    }, ],
-});
-const user = mongoose.model("user", userschema);
+// const userschema = new mongoose.Schema({
+//     name: String,
+//     email: { type: String, unique: true, required: true },
+//     password: String,
+//     products: [{
+//         productRef: {
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "product"
+//         },
+//         count: Number
+//     }],
+//     cart: [{
+//         productRef: {
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "product"
+//         },
+//         count: Number
+//     }, ],
+// });
+// const user = mongoose.model("user", userschema);
 
 app.get("/", (req, res) => {
     res.send("IT'S WORKING!!");
@@ -91,27 +95,27 @@ app.post("/product", (req, res) => {
     let count = body["count"];
     let description = body["description"];
     let pr = { name: name, price: price, brand: brand, type: type, category: category, count: count, image: image, description: description };
-    // client.connect(err => {
-    //     const collection = client.db("smart-shop-db").collection("product");
-    //     // perform actions on the collection object
-    //     collection.create(pr, (err, newly) => {
-    //         if (err) {
-    //             console.log(err);
-    //             res.status(500).json({ error: err });
-    //         } else {
-    //             res.status(200).json({ data: newly });
-    //         }
-    //     })
-    //     client.close();
-    // });
-    product.create(pr, (err, newly) => {
-        if (err) {
-            console.log(err);
-            res.status(500).json({ error: err });
-        } else {
-            res.status(200).json({ data: newly });
-        }
-    })
+    client.connect(err => {
+        const collection = client.db("smart-shop-db").collection("product");
+        // perform actions on the collection object
+        collection.create(pr, (err, newly) => {
+            if (err) {
+                console.log(err);
+                res.status(500).json({ error: err });
+            } else {
+                res.status(200).json({ data: newly });
+            }
+        })
+        client.close();
+    });
+    // product.create(pr, (err, newly) => {
+    //     if (err) {
+    //         console.log(err);
+    //         res.status(500).json({ error: err });
+    //     } else {
+    //         res.status(200).json({ data: newly });
+    //     }
+    // })
 });
 app.listen(process.env.PORT || 3000, () => {
     console.log(`server started at ${3000}`);
