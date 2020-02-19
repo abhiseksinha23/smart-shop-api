46c20d095c802ebd620eb24940134a21bea267d4
Abhisek Kumar <42487773+abhiseksinha23@users.noreply.github.com>
2020-02-20 02:38:37 +0530

    user schema

--- schema/user.js
+++ schema/user.js
@@ -3,20 +3,21 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: String,
     email: { type: String, unique: true, required: true },
+    profilePicUrl: String,
     password: String,
     products: [{
         productRef: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "product"
         },
-        count: Number
+        count: { type: Number, default: 0 }
     }],
     cart: [{
         productRef: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "product"
         },
-        count: Number
+        count: { type: Number, default: 0 }
     }, ],
 });
 
