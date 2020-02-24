33a2e5c2c3faea735bbc1de57f3d24960f6cd780
Abhisek Kumar <42487773+abhiseksinha23@users.noreply.github.com>
2020-02-25 00:25:50 +0530

    cart added 13

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const { userSchema } = require('./schema/user');
 const { cartschema } = require('./schema/cart');
 const product = mongoose.model("product", productSchema);
 const user = mongoose.model("user", userSchema);
-const cart = mongoose.model("user", cartschema);
+const cart = mongoose.model("cart", cartschema);
 
 //////////////////////////////////////////////////////////////////
 
@@ -169,20 +169,20 @@ app.post("/createUser", (req, res) => {
 ///////////////////////////////////////////////////////////
 //CART ROUTES
 
-// app.post("/addtocart/:userid", (req, res) => {
-//     let userid = req.params.userid;
-//     let productRef = req.body.productRef;
-//     let count = req.body.count;
-//     let cr = { userid: userid, productRef: productRef, count: count };
-//     cart.create(cr, (err, newly) => {
-//         if (err) {
-//             console.log(err);
-//             res.status(500).json({ error: err });
-//         } else {
-//             res.status(200).json({ data: newly });
-//         }
-//     })
-// });
+app.post("/addtocart/:userid", (req, res) => {
+    let userid = req.params.userid;
+    let productRef = req.body.productRef;
+    let count = req.body.count;
+    let cr = { userid: userid, productRef: productRef, count: count };
+    cart.create(cr, (err, newly) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: err });
+        } else {
+            res.status(200).json({ data: newly });
+        }
+    })
+});
 // app.get("/cart/:userId", (req, res) => {
 //     let userId = req.params.userId;
 //     user.find({ userid: userId }, (err, user) => {
