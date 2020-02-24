480f95e7d91fde47c63db1efe4ddf04e4405f3aa
Abhisek Kumar <42487773+abhiseksinha23@users.noreply.github.com>
2020-02-25 00:49:31 +0530

    cart added 13

--- app.js
+++ app.js
@@ -174,15 +174,15 @@ app.post("/addtocart/:userid", (req, res) => {
     let userid = req.params.userid;
     let productRef = req.body.productRef;
     let count = req.body.count;
-    let cr = { userid: userid, productRef: productRef, count: count };
-    cart.create(cr, (err, newly) => {
-        if (err) {
-            console.log(err);
-            res.status(500).json({ error: err });
-        } else {
-            res.status(200).json({ data: newly });
-        }
-    })
+    // let cr = { userid: userid, productRef: productRef, count: count };
+    // cart.create(cr, (err, newly) => {
+    //     if (err) {
+    //         console.log(err);
+    //         res.status(500).json({ error: err });
+    //     } else {
+    //         res.status(200).json({ data: newly });
+    //     }
+    // })
     let a = { productRef: req.body.productRef, count: req.body.count };
     user.find({ userid: userid }, (err, us) => {
         if (err) {
