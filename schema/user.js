07cc44daa00bd70f44d80cd916d4f255e6234abd
Abhisek Kumar <42487773+abhiseksinha23@users.noreply.github.com>
2020-02-28 02:52:34 +0530

    required

--- schema/user.js
+++ schema/user.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: String,
+    name: { type: String, required: true },
     userid: { type: String, unique: true, required: true },
     email: { type: String, unique: true, required: true },
-    profilePicUrl: String,
+    profilePicUrl: { type: String, required: true },
     products: [{
         productRef: {
             type: mongoose.Schema.Types.ObjectId,
