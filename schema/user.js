4ff698f42e090b6758d70bdad5004e223ae2ece9
Abhisek Kumar <42487773+abhiseksinha23@users.noreply.github.com>
2020-02-29 11:23:03 +0530

    user changes

--- schema/user.js
+++ schema/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    name: { type: String, default: "user" },
     userid: { type: String, unique: true, required: true },
     email: { type: String, unique: true, required: true },
     profilePicUrl: { type: String, default: "https://image.flaticon.com/icons/svg/145/145848.svg" },
